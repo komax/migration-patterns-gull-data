be22a89a8fbe2d37b5c1585280916bd810fc8f31
Maximilian Konzack <m.p.konzack@tue.nl>
2016-11-04 17:05:50 +0100

    Added open layer type definitions and updated the link from tooltipster to jquery

--- Main/libraries/definitions/jquery.tooltipster.d.ts
+++ Main/libraries/definitions/jquery.tooltipster.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: Patrick Magee <https://github.com/pjmagee/>, Dmitry Pesterev <https://github.com/VorobeY1326/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/// <reference path="../jquery/jquery.d.ts" />
+/// <reference path="./jquery.d.ts" />
 
 interface JQueryTooltipsterOptions {
     /**
