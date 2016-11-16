3beba2b1ee9ae3eb1a145324c699bfbddfbf28b3
Maximilian Konzack <m.p.konzack@tue.nl>
2016-11-16 14:44:06 +0100

    Moved maps.js to typescript

--- Main/resources/maps.ts
+++ Main/resources/maps.ts
@@ -1,26 +1,31 @@
 /// <reference path="../libraries/definitions/openlayers.d.ts" />
 /// <reference path="../libraries/definitions/jquery.d.ts" />
+
 /**
  * Map setup and configuration
  */
+
 (function (global) {
     $(function () {
+
         var maps = {};
-        //------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+
         var layers = maps.layers = {
             mapquestLeft: new ol.layer.Tile({
-                source: new ol.source.MapQuest({ layer: 'sat' }),
-                minResolution: 50
+                source: new ol.source.MapQuest({layer: 'sat'}),
+                minResolution: 50,
             }),
             mapquestRight: new ol.layer.Tile({
-                source: new ol.source.MapQuest({ layer: 'sat' }),
-                minResolution: 50
+                source: new ol.source.MapQuest({layer: 'sat'}),
+                minResolution: 50,
             }),
             topologyLeft: new ol.layer.Tile({
-                source: new ol.source.MapQuest({ layer: 'hyb' })
+                source: new ol.source.MapQuest({layer: 'hyb'}),
             }),
             topologyRight: new ol.layer.Tile({
-                source: new ol.source.MapQuest({ layer: 'hyb' })
+                source: new ol.source.MapQuest({layer: 'hyb'}),
             }),
             positron: new ol.layer.Tile({
                 source: new ol.source.XYZ({
@@ -32,53 +37,62 @@
                 title: 'Global Imagery',
                 source: new ol.source.TileWMS({
                     url: 'http://demo.opengeo.org/geoserver/wms',
-                    params: { LAYERS: 'ne:NE1_HR_LC_SR_W_DR', VERSION: '1.1.1' }
+                    params: {LAYERS: 'ne:NE1_HR_LC_SR_W_DR', VERSION: '1.1.1'}
+                    //params: { LAYERS: 'nasa:bluemarble', VERSION: '1.1.1' }
                 })
             }),
             journey: Journey.main.layer,
-            schematic: Schematic.main.layer
+            schematic: Schematic.main.layer,
         };
+
         maps.view = new ol.View({
             center: ol.proj.fromLonLat([-0.234747, 38.0422329]),
-            zoom: 4.25
+            zoom: 4.25,
         });
-        // For heatmap
+
+// For heatmap
         maps.left = new ol.Map({
             controls: ol.control.defaults()
                 .extend([
-                new window.interface.expandControl('#left-view', '#right-view', '_|', '_|_'),
-                new window.interface.settingsControl('#left-toolbar', '#right-view', '_|', '_|_'),
-                new ol.control.ScaleLine(),
-            ]),
+                    new window.interface.expandControl('#left-view', '#right-view', '_|', '_|_'),
+                    new window.interface.settingsControl('#left-toolbar', '#right-view', '_|', '_|_'),
+                    new ol.control.ScaleLine(),
+                ]),
             target: 'left-map',
             layers: [layers.positron, layers.mapquestLeft]
                 .concat(global.heatmap.layers)
                 .concat([layers.topologyLeft, Journey.main.layer]),
-            view: maps.view
+            view: maps.view,
         });
-        // For migration map
+
+// For migration map
         maps.right = new ol.Map({
             controls: ol.control.defaults()
                 .extend([
-                new window.interface.expandControl('#right-view', '#left-view', '|_', '_|_'),
-                new window.interface.settingsControl('#right-toolbar', '#right-view', '_|', '_|_'),
-                new window.interface.paneControl('#pane', '45%', '=', '_'),
-                new ol.control.ScaleLine(),
-            ]),
+                    new window.interface.expandControl('#right-view', '#left-view', '|_', '_|_'),
+                    new window.interface.settingsControl('#right-toolbar', '#right-view', '_|', '_|_'),
+                    new window.interface.paneControl('#pane', '45%', '=', '_'),
+                    new ol.control.ScaleLine(),
+                ]),
             target: 'right-map',
             layers: [layers.positron, layers.mapquestRight, layers.topologyRight, layers.schematic],
             view: maps.view,
             interactions: ol.interaction.defaults()
-                .extend([Schematic.main.select])
+                .extend([Schematic.main.select]),
         });
-        //------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+
         maps.resize = function resize() {
             maps.left.updateSize();
             maps.right.updateSize();
-        };
-        //------------------------------------------------------------------------------
+        }
+
+//------------------------------------------------------------------------------
+
         global.Maps = maps;
+
     });
 })(window || this);
+
 //------------------------------------------------------------------------------
-//# sourceMappingURL=maps.js.map
