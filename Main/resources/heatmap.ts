1be2098e918a57f2eb148c9341e5ba31b07de32c
Maximilian Konzack <m.p.konzack@tue.nl>
2016-11-16 14:31:05 +0100

    Heatmap as proper ts class

--- Main/resources/heatmap.ts
+++ Main/resources/heatmap.ts
@@ -4,15 +4,12 @@
  * A map layer that shows the complete journey of a gull segmented on day/night
  */
 
-interface String {
-    hashCode: () => number;
-}
 
 (function (global) {
 
 //------------------------------------------------------------------------------
 
-    var defaults = {
+    let defaults = {
         colors: ['#ffffb2', '#fecc5c', '#fd8d3c', '#f03b20', '#bd0026'],
         style: function (color) {
             return new ol.style.Style({
@@ -27,87 +24,92 @@ interface String {
         heatmap_webserver: 'http://localhost:5000/'
     };
 
-    var Heatmap = function Heatmap() {
-        this.sources = [];
-        this.layers = [];
+    class Heatmap {
+        private sources: Array<ol.source.Vector>;
+        private layers: Array<ol.layer.Vector>;
 
-        for (var i = 0; i < 5; i++) {
-            this.sources.push(new ol.source.Vector({}));
-            this.sources[i].addFeatures(defaults.features(i + 1));
-            this.layers.push(new ol.layer.Vector({
-                source: this.sources[i],
-                style: defaults.style(defaults.colors[i])
-            }));
+        constructor() {
+            this.sources = [];
+            this.layers = [];
+
+            for (let i = 0; i < 5; i++) {
+                this.sources.push(new ol.source.Vector({}));
+                this.sources[i].addFeatures(defaults.features(i + 1));
+                this.layers.push(new ol.layer.Vector({
+                    source: this.sources[i],
+                    style: defaults.style(defaults.colors[i])
+                }));
+            }
+        };
+
+        loadHeatmapFrom(url, fallback) {
+            $.ajax({
+                url: url,
+                dataType: 'jsonp',
+                crossDomain: true,
+                jsonpCallback: 'jsonpContours',
+                success: function (data) {
+                    let contourArray = [data.contour1, data.contour2, data.contour3, data.contour4, data.contour5];
+
+                    for (let i = 0; i < this.sources.length; i++) {
+                        let geojson = new ol.format.GeoJSON(),
+                            features = geojson.readFeatures(contourArray[i]);
+                        this.sources[i].clear();
+                        this.sources[i].addFeatures(features);
+                    }
+
+                    console.log("Succesfully loaded heatmap.")
+                },
+                error: function (xhr, status, error) {
+                    if (fallback) {
+                        this.loadHeatmapFrom(fallback);
+                    } else {
+                        console.log(error);
+                    }
+                    //console.log("Could not retrieve heatmap.")
+                }
+            });
         }
-    };
+
+        load(ids) {
+            let safe_ids = ids.slice(0);
+            let restful_url = defaults.heatmap_webserver + safe_ids.join('$');
+            let directory_url = defaults.heatmap_local + hashCode(safe_ids.sort().join("")) + '.geojsonp';
+            this.loadHeatmapFrom(directory_url, restful_url);
+        }
+
+        setOpacity(val) {
+            for (let layer of this.layers) {
+                layer.setOpacity(val);
+            }
+        }
+
+        clear() {
+            for (let i = 0; i < this.layers.length; i++) {
+                this.sources[i].clear();
+                this.sources[i].addFeatures(defaults.features(i + 1));
+            }
+        }
+    }
 
 // from: http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
-    String.prototype.hashCode = function () {
-        var hash = 0;
+    function hashCode(strInput: string): number {
+        let hash = 0;
         if (this.length === 0) {
             return hash;
         }
-        for (let i = 0; i < this.length; i++) {
-            let char = this.charCodeAt(i);
+        for (let i = 0; i < strInput.length; i++) {
+            let char = strInput.charCodeAt(i);
             hash = ((hash << 5) - hash) + char;
             hash = hash & hash;
         }
         return hash >>> 0; // we want unsigned.. couldn't get the python part to work otherwise..
-    };
+    }
 
-    Heatmap.prototype.loadHeatmapFrom = function (url, fallback) {
-        var self = this;
-        $.ajax({
-            url: url,
-            dataType: 'jsonp',
-            crossDomain: true,
-            jsonpCallback: 'jsonpContours',
-            success: function (data) {
-                var contourArray = [data.contour1, data.contour2, data.contour3, data.contour4, data.contour5]
-
-                for (var i = 0; i < self.sources.length; i++) {
-                    var geojson = new ol.format.GeoJSON(),
-                        features = geojson.readFeatures(contourArray[i]);
-                    self.sources[i].clear();
-                    self.sources[i].addFeatures(features);
-                }
-
-                console.log("Succesfully loaded heatmap.")
-            },
-            error: function (xhr, status, error) {
-                if (fallback) {
-                    self.loadHeatmapFrom(fallback);
-                } else {
-                    console.log(error);
-                }
-                //console.log("Could not retrieve heatmap.")
-            }
-        });
-    };
-
-    Heatmap.prototype.load = function load(ids) {
-        var safe_ids = ids.slice(0);
-        var restful_url = defaults.heatmap_webserver + safe_ids.join('$');
-        var directory_url = defaults.heatmap_local + safe_ids.sort().join("").hashCode() + '.geojsonp';
-        this.loadHeatmapFrom(directory_url, restful_url);
-    };
-
-    Heatmap.prototype.setOpacity = function setOpacity(val) {
-        for (var i = 0; i < this.layers.length; i++) {
-            this.layers[i].setOpacity(val);
-        }
-    };
-
-    Heatmap.prototype.clear = function clear() {
-        for (var i = 0; i < this.layers.length; i++) {
-            this.sources[i].clear();
-            this.sources[i].addFeatures(defaults.features(i + 1));
-        }
-    };
 
 //------------------------------------------------------------------------------
 
-    (<any>global).Heatmap = Heatmap;
+    (<any>global).Heatmap = {};
     (<any>global).Heatmap.main = new Heatmap();
 
 })(window || this);
