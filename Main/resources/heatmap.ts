3ffd72b833621af5b9038af25adea0efc0a8c736
Maximilian Konzack <m.p.konzack@tue.nl>
2017-01-04 14:51:24 +0100

    Show legend as attribution of the corresponding map

--- Main/resources/heatmap.ts
+++ Main/resources/heatmap.ts
@@ -24,82 +24,97 @@ namespace MigrationVisualization {
         heatmap_webserver: 'http://localhost:5000/'
     };
 
-    function showHeatmapLegend() {
-        const palette = defaults.colors;
+    export function heatmapLegend(): string {
+        let heatmapLegendHTMLcode: string = "";
 
-        const data: any[] = [];
-        let offset = 0;
-        const rectWidth = 20;
+        function computeLegend(): string {
+            const palette = defaults.colors;
 
-        for (let i = 0; i < palette.length; i++) {
-            data.push({x: offset, w: rectWidth});
-            offset += rectWidth;
+            const data: any[] = [];
+            let offset = 0;
+            const rectWidth = 20;
+
+            for (let i = 0; i < palette.length; i++) {
+                data.push({x: offset, w: rectWidth});
+                offset += rectWidth;
+            }
+
+
+            const height = 30, width = 180;
+            const margin = {top: 2, right: 2, bottom: 2, left: 2},
+                innerWidth = width - margin.left - margin.right,
+                innerHeight = height - margin.top - margin.bottom;
+
+            // Create the svg element.
+            const svg = d3.select("#heatmap-legend").append("svg")
+                .attr("width", innerWidth + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+            // Set up the scale to correspond to the rectWidths.
+            const x = d3.scale.linear()
+                .rangeRound([0, innerWidth])
+                .domain([0, palette.length * rectWidth - 1]);
+
+            // Enter the data with the corresponding color.
+            svg.selectAll("rect")
+                .data(data)
+                .enter()
+                .append("rect")
+                .attr("width", (d) => {
+                    return x(d.w);
+                })
+                .attr("height", innerHeight)
+                .attr("x", (d) => {
+                    return x(d.x);
+                })
+                .attr("y", margin.top)
+                .style("fill", (d, i) => {
+                    return palette[i];
+                });
+
+            // Add text labels to the columns.
+            svg.selectAll("text")
+                .data(data)
+                .enter()
+                .append("text")
+                .text((d, i) => {
+                    if (i === 0) {
+                        return "Less";
+                    } else if (i === palette.length - 1) {
+                        return "More";
+                    } else {
+                        return "";
+                    }
+                })
+                .attr("x", (d) => {
+                    return x(d.w / 2 + d.x);
+                })
+                .attr("y", margin.top + innerHeight / 2)
+                .attr("font-size", 15)
+                .style("text-anchor", "middle")
+                .style("alignment-baseline", "middle")
+                .style("fill", "#ffffff")
+                .style("stroke", "#000000")
+                .style("stroke-width", 1)
+                .style("font-weight", "bold");
+
+            // Generate the html code for the legend.
+            const htmlCode: string = $("#heatmap-legend").html();
+
+            // Remove the temporary svg element.
+            d3.select(`#heatmap-legend svg`).remove();
+            return htmlCode;
         }
-        console.log(data);
 
-
-        const height = 30, width = 180;
-        const margin = {top: 2, right: 2, bottom: 2, left: 2},
-            innerWidth = width - margin.left - margin.right,
-            innerHeight = height - margin.top - margin.bottom;
-
-        // Create the svg element.
-        const svg = d3.select("#heatmap-legend").append("svg")
-            .attr("width", innerWidth + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-        // Set up the scale to correspond to the rectWidths.
-        const x = d3.scale.linear()
-            .rangeRound([0, innerWidth])
-            .domain([0, palette.length * rectWidth - 1]);
-
-        // Enter the data with the corresponding color.
-        svg.selectAll("rect")
-            .data(data)
-            .enter()
-            .append("rect")
-            .attr("width", (d) => {
-                return x(d.w);
-            })
-            .attr("height", innerHeight)
-            .attr("x", (d) => {
-                return x(d.x);
-            })
-            .attr("y", margin.top)
-            .style("fill", (d, i) => {
-                return palette[i];
-            });
-
-        // Add text labels to the columns.
-        svg.selectAll("text")
-            .data(data)
-            .enter()
-            .append("text")
-            .text((d, i) => {
-                if (i === 0) {
-                    return "Less";
-                } else if (i === palette.length - 1) {
-                    return "More";
-                } else {
-                    return "";
-                }
-            })
-            .attr("x", (d) => {
-                return x(d.w / 2 + d.x);
-            })
-            .attr("y", margin.top + innerHeight / 2)
-            .attr("font-size", 15)
-            .style("text-anchor", "middle")
-            .style("alignment-baseline", "middle")
-            .style("fill", "#ffffff")
-            .style("stroke", "#000000")
-            .style("stroke-width", 1)
-            .style("font-weight", "bold");
-
-        console.log($("#heatmap-legend").html());
-
+        $(document).ready(() => {
+            heatmapLegendHTMLcode = computeLegend();
+        });
+        if (heatmapLegendHTMLcode === "") {
+            heatmapLegendHTMLcode = computeLegend();
+        }
+        return heatmapLegendHTMLcode;
     }
 
     export class Heatmap {
@@ -118,11 +133,6 @@ namespace MigrationVisualization {
                     style: defaults.style(defaults.colors[i])
                 }));
             }
-
-            $(document).ready(() => {
-                // Paint the legend for the heatmap.
-                showHeatmapLegend();
-            });
         }
 
         loadHeatmapFrom(url: string, fallback: string) {
